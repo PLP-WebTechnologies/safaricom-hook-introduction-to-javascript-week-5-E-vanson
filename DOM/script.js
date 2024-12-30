a9fc0cdfea35b4e0db3a6daf55b464faba2d74bf
Evans <evansonkamau233@gmail.com>
2024-12-30 18:04:29 +0300

    Data Types Created

---
+++ DOM/script.js
@@ -0,0 +1,26 @@
+const namee = "Njung'e Evanson Kamau";
+console.log("The devs' name ", namee);
+
+let age = 21;
+console.log("The devs' age ", age);
+
+const occupation = "Student";
+console.log("The devs' occupation ", occupation);
+
+const institution = "Chuka University";
+console.log("The devs' institution ", institution);
+
+const isEmployed = false;
+console.log("Is the dev employed ", isEmployed);
+
+const detailsArr = ["Njung'e Evanson Kamau", 21, "Student", "Chuka University", false];
+console.log("Array with all devs' details ", detailsArr);
+
+const detailsObj = {
+    name: "Njung'e Evanson Kamau",
+    age: 21,
+    occupation: "Student",
+    institution: "Chuka University",
+    isEmployed: false
+};
+console.log("Object with all devs details ", detailsObj);
